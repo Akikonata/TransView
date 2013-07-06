0678ffcc239bd431add12605acf9c90e9973f37b
Pan <panzheng@admaster.com.cn>
2013-07-06 21:15:56 +0800

    added lib

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-(function(eve){
+(function(){
   var _root = this; 
   var nav_bar = $("#nav-bar");
-  var CRMModel={};
+  var CRMModel = {};
   nav_bar.on("click","li",function(){
     var o = $(this);
     o.siblings().removeClass("active");
     o.addClass("active"); 
-    nav_bar.siblings.remove();
+    nav_bar.siblings().remove();
+    nav_bar.after($("#tpl-"+o.data("module")).html());
   });
-  CRMModel.getAccounts = function(cb){
-    $.getJSON("",
+  CRMModel.getSNAccounts = function(cb){
+    $.getJSON(serverHost+"/social_network/valid_accounts/?network_id="+network_id+"&group_id="+group_id,
       {},
       cb(data));
   }
