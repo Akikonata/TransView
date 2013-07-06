93a02e5c17eeed6f4cae84e69bb03e8f1ec063f8
Pan <panzheng@admaster.com.cn>
2013-07-07 02:32:14 +0800

    added weibo list

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
 (function(){
   var _root = this; 
   var bottom_bar = $(".bottom-bar");
-
+  var CRM_Status = {};
+  CRM_Status.tmp = _.template($("#Tmp-status").html().replace(/[\f\n\r\t\v]/g,""));
+  CRM_Status.get_user_link = function(platform,u_id){
+          var url = null;
+          switch(platform){
+            case "sina": url = "http://weibo.com/u/" + u_id; break;
+            case "tencent":url = "http://t.qq.com/"+ u_id; break;
+            default: break;
+          }
+          return url;
+        }
+  CRM_Status.get_status_link = function(platform,u_id,s_id){
+          var url = null;
+          switch(platform){
+            case "sina": url = "http://api.t.sina.com.cn/" + u_id + "/statuses/" + s_id; break;
+            case "tencent": url = "http://t.qq.com/p/t/" + s_id; break;
+            default: break;
+          }
+          return url;
+        }
+  CRM_Status.verify_map = {
+      0:"approve",// 0-名人；
+      1:"approveco",// 1-政府;
+      2:"approveco",// 2-企业；
+      3:"approveco",// 3-媒体；
+      4:"approveco",// 4-校园；
+      5:"approveco",// 5-网站；
+      6:"approveco",// 6-应用；
+      7:"approveco",// 7-团体（机构）；
+      8:"none",// 8-待审企业；
+      10:"vlady",// 10-微女郎 
+      200:"club",// 200-初级达人；
+      220:"club",// 220-通过审核的达人；
+      400:"approve" // 400-已故V用户
+    };
+  CRM_Status.verify_map[-1]="none";// -1:普通用户
   var sn_timeline_urls = {
     home_statuses:"/home_timeline/statuses/conditions/",
     mention_statuses:"/mentions_in_statuses/conditions/",
@@ -28,4 +63,5 @@
       cb);
   }
   _root.CRMModel = CRMModel;
+  this.CRM_Status = CRM_Status;
 })(window);
--- socialnet.js
+++ socialnet.js
@@ -4,9 +4,16 @@
 	var weibo_pane = $("#weibo-pane");
 	var weibo_list = weibo_pane.find(".weibo-list");
 	var timeline_select = $("#timeline-select");
-	var startTime = Date.parse(0);
-	var endTime = Date.parse(new Date());
-  var d_conf = {network_id:network_id,group_id:group_id}; 
+	var startTime = Math.round(Date.parse(0)/1000);
+	var endTime = Math.round(Date.parse(new Date())/1000);
+  var d_conf = {network_id:network_id,group_id:group_id,start_time:startTime,end_time:endTime}; 
+  var permission_tpl = $($("#tpl-permission").html());
+  var permission = {
+    monitor:permission_tpl.find(".monitor").text()==="true"?true:false,
+    comment:permission_tpl.find(".comment").text()==="true"?true:false,
+    repost:permission_tpl.find(".repost").text()==="true"?true:false, 
+    assign:permission_tpl.find(".assign").text()==="true"?true:false,
+  };
 	CRMModel.getSNAccounts(function(data){
 		console.log(data);
 		data.forEach(function(o){
@@ -21,16 +28,89 @@
 
 	//时间线选择器变化时候获取相应微博
 	timeline_select.on("change",this,function(){
+		var o = $(this);  
+		var timeline = o.val();
+		CRMModel.getSNWeiboList(timeline,d_conf,function(data){
+        console.log(data);
+        var is_comment = (timeline.indexOf("comments") > -1),
+            is_accounts_statuses = (timeline.indexOf("accounts_statuses")>-1),
+            is_comments_by_me = (timeline.indexOf("comments_by_me") > -1),
+            is_mention_statuses =(timeline.indexOf("mention_statuses") > -1);
+        var statuses = data.statuses;
 
+        statuses.forEach(function(o){
+          var user_link = CRM_Status.get_user_link(o.platform,o.user.uid);
+          var retweet_user_link = CRM_Status.get_user_link(o.platform,o.retweet.uid);
+          var s_link = CRM_Status.get_status_link(o.platform,o.user.uid,o._id);
+          var retweet_s_link = CRM_Status.get_status_link(o.platform,o.retweet.uid,o.retweet.id);
+          //渲染单条微博
+          var _tmp = CRM_Status.tmp(
+            {
+              id:o._id,
+              is_comment: is_comment,
+              u_id : o.user.uid,
+              u_link : user_link,
+              u_avatar : o.user.uimg,
+              u_name: o.user.name,
+              u_verify: o.user.isV?o.user.verified_type:-1,
+              s_text: o.text,
+              s_pic: o.pic,
+              s_link: s_link,
+              c_date: o.cdate,
+              s_source:o.source,
+              platform: o.platform,
+              action_source:"account",
+              data_type : d_conf.request,
+              action_send_id : d_conf.account_id,
+              is_deal:o.is_deal,
+              pan:null,
+              logs:o.logs,
+              func_config:{
+                monitor:(d_conf.platform!=="sina"||is_comment||o.retweet.id||!permission.monitor)?
+                        null:{
+                          state:o.is_monitor
+                        },
+                flash:o.flash,
+                eyeball:o.eyeball,
+                comment:is_comments_by_me?
+                        null:{
+                          count:o.ccount,
+                          func:permission.comment&&!o.is_deal
+                        },
+                repost:is_comment?
+                        null:{
+                          count:o.rcount,
+                          func:permission.repost&&!o.is_deal
+                        },
+                assign:permission.assign&&!o.is_deal&&!is_comments_by_me && !is_accounts_statuses
+              },
+              repost_block:o.retweet.id?
+                          {
+                            id:o.retweet.id,
+                            u_name:o.retweet.s_name,
+                            u_link:retweet_user_link,
+                            s_text:o.retweet.text,
+                            s_pic:o.retweet.pic,
+                            s_link:retweet_s_link,
+                            c_date:o.retweet.date,
+                            s_source:o.retweet.source
+                          }:null,
+            }
+          );
+          weibo_list.append(_tmp);
+        });
+        $("a[data-toggle='tooltip']").tooltip();
+      });
 	});
 	//触发获取微博的事件
 	account_list.on("click",".btn",function(){
 		var o = $(this);
 		d_conf.account_id = o.data("id");
+		d_conf.platform = o.data("platform");
 		o.siblings().removeClass("active");
 		o.addClass("active");
 		account_pane.slideUp();
 		weibo_pane.slideDown();
-		imeline_select.trigger("change");
+		timeline_select.trigger("change");
 	}); 
 })();
