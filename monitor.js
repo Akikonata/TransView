8ea8c74599f0810d201f28ff7d59d2974eb7d388
Pan <panzheng@admaster.com.cn>
2013-07-07 07:04:44 +0800

    added monitor pane

--- monitor.js
+++ monitor.js
@@ -14,7 +14,43 @@
     var weibo_id = o.val();
     CRMModel.getMNWeiboDetail(weibo_id,function(data){
       console.log(data);
-
+      var o = data;
+      var user_link = CRM_Status.get_user_link(o.platform,o.user.uid);
+      var s_link = CRM_Status.get_status_link(o.platform,o.user.uid,o._id);
+      var _tmp = CRM_Status.tmp(
+            {
+              id:o._id,
+              is_comment: false,
+              u_id : o.user.uid,
+              u_link : user_link,
+              u_avatar : o.user.profile_image_url,
+              u_name: o.user.name,
+              u_verify: o.user.isV?o.user.verified_type:-1,
+              s_text: o.text,
+              s_pic: o.pic,
+              s_link: s_link,
+              c_date: o.cdate,
+              s_source:o.source,
+              platform: o.platform,
+              action_source:null,
+              data_type : null,
+              action_send_id : null,
+              is_deal:null,
+              pan:null,
+              logs:o.logs,
+              func_config:{
+                monitor:null,
+                flash:o.flash,
+                eyeball:o.eyeball,
+                comment:{count:o.comments_count},
+                repost:{count:o.reposts_count}
+              },
+              repost_block:null,
+            }
+          );
+      trans_detail.find(".Status-single").remove();
+      trans_detail.append(_tmp);
+      $("a[data-toggle='tooltip']").tooltip();
     });
   });
 })();
