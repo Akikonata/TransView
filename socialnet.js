deb38b26f087bd02f25f75a154d083e32c713581
Pan <panzheng@admaster.com.cn>
2013-07-06 23:23:45 +0800

    test

--- socialnet.js
+++ socialnet.js
@@ -6,6 +6,7 @@
 			var _tmp = "<li class='btn account' data-id='" + o._id + "' data-platform='"+o.platform+"'>"+
                       "<img class='avatar' src ='" + o.profile_image_url + "'/>"+
                       "<span>" + o.screen_name + "</span>"+
+                      
                       "<img src='icon16_" + o.platform + ".png' class='platform-icon'/>"+
                    "</li>";
             account_list.prepend($(_tmp));
