126bb55b8432b8a423f3c41b99c02e7009f01f04
Pan <panzheng@admaster.com.cn>
2013-07-06 21:47:54 +0800

    added files

--- main.js
+++ main.js
@@ -2,13 +2,6 @@
   var _root = this; 
   var nav_bar = $("#nav-bar");
   var CRMModel = {};
-  nav_bar.on("click","li",function(){
-    var o = $(this);
-    o.siblings().removeClass("active");
-    o.addClass("active"); 
-    nav_bar.siblings().remove();
-    nav_bar.after($("#tpl-"+o.data("module")).html());
-  });
   CRMModel.getSNAccounts = function(cb){
     $.getJSON(serverHost+"/social_network/valid_accounts/?network_id="+network_id+"&group_id="+group_id,
       {},
