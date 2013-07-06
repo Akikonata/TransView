b31e7bd68721ab01e8f615f3ff99b20ff8caf0f4
Pan <panzheng@admaster.com.cn>
2013-07-06 20:51:54 +0800

    added serverHost

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-(function(){
+(function(eve){
+  var _root = this; 
   var nav_bar = $("#nav-bar");
+  var CRMModel={};
   nav_bar.on("click","li",function(){
     var o = $(this);
     o.siblings().removeClass("active");
     o.addClass("active"); 
-  });  
-
-  $("body").on("touchmove",this,function(){
-    e.stopPropagation();
-    e.preventDefault();
-  }); 
-})();
+    nav_bar.siblings.remove();
+  });
+  CRMModel.getAccounts = function(cb){
+    $.getJSON("",
+      {},
+      cb(data));
+  }
+  
+  _root.CRMModel = CRMModel;
+})(window);
