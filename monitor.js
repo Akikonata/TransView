9252cbb1f1ce5fab9cb6821ac67e2651cc9cb316
Pan <panzheng@admaster.com.cn>
2013-07-07 08:23:21 +0800

    added travel tend

--- monitor.js
+++ monitor.js
@@ -1,6 +1,12 @@
 (function(){
   var status_select = $("#status-select");
   var trans_detail = $("#trans-detail");
+  var T_dist_data = {
+    created_at:null,
+    data_hour:null,
+    data_day:null
+  }
+
   CRMModel.getMNWeibo(function(data){
     var data_list = data.data_list;
         status_select.empty();
@@ -51,7 +57,51 @@
       trans_detail.find(".Status-single").remove();
       trans_detail.prepend(_tmp);
       $("a[data-toggle='tooltip']").tooltip();
+
+      //绘制传播图
+      var createDate = Date.parse(data.created_at);
+      var hour_dist = data.user_dist.hour_dist;
+      var configTend ={
+                    colors: ['#1C77C1', '#E04200', '#FEB800', '#00A3E8', '#714286', '#EA5503'],
+                    chart: {
+                      zoomType: 'x'
+                    },
+                    title:{
+                      text:"",
+                    },
+                    series: [{
+                      name: '转发量',
+                      data: null,
+                    }],
+                    xAxis: {
+                      type: 'datetime',
+                      maxZoom: 24 * 3600 * 1000,
+                      title:{
+                        text:null,
+                        align:"left"
+                      }
+                    },
+                    yAxis: {
+                      title:"",
+                      min:0,
+                      margin:[0,0,0,-40]
+                    },
+                    exporting:{
+                      enabled: false
+                    },
+                    credits:{
+                      enabled:false
+                    }
+                };
+        var timeLocal = new Date().getTimezoneOffset() * 60 * 1000;
+        configTend.series[0].data = hour_dist; 
+        configTend.series[0].pointStart = createDate - timeLocal;
+        configTend.series[0].pointInterval = 3600 * 1000; // one hour
+        configTend.xAxis.title.text = "小时";
+
+      $("#travel-tend").highcharts(configTend);
     });
+
     CRMModel.getMNTransmission(weibo_id,function(data){
       $("#travel-path").replaceWith($("#travelpath-tpl").html());
       var travel_map = $("#travel-path");
